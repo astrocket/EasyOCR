663932562520a9f984893cf94051fe5abb9328bc
byungwook <bwson98@gmail.com>
2020-08-26 15:08:01 +0900

    delete unnecessary code.

--- app_gpu.py
+++ app_gpu.py
@@ -1,18 +1,14 @@
 import os
-from flask import Flask, request, send_file, flash, redirect, render_template, url_for, jsonify
+from flask import Flask, request, send_file, redirect, render_template
 import easyocr
-from werkzeug.utils import secure_filename
 import PIL
 from PIL import Image, ImageOps
 import base64
-import cv2
 import numpy as np
-import tempfile
 import io
 from queue import Queue, Empty
 import time
 import threading
-
 
 
 UPLOAD_FOLDER = 'static/uploads'
@@ -21,10 +17,10 @@ app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024*5
 
-############
 requests_queue = Queue()
 BATCH_SIZE = 1
 CHECK_INTERVAL = 0.1
+
 
 def handle_requests_by_batch():
     while True:
@@ -40,18 +36,22 @@ def handle_requests_by_batch():
 
             for request, output in zip(requests_batch, batch_outputs):
                 request['output'] = output
-                
+
+
 threading.Thread(target=handle_requests_by_batch).start()
 
+
 def run(file, lang):
-    
     imgFile = np.array(PIL.Image.open(file).convert("RGB"))
+
     if lang == 'eng':
         reader = easyocr.Reader(['en'])
     else:
         reader = easyocr.Reader([lang, 'en'])
+
     text = reader.readtext(imgFile)
     res_list = list()
+
     for i in text:
         res_list.append(i[1])
     
@@ -60,28 +60,24 @@ def run(file, lang):
     for i in res_list:
         res_str = res_str + ' ' + i 
 
-    #image show
     imgFile = PIL.Image.fromarray(imgFile)
     img_io = io.BytesIO()
     imgFile.save(img_io, 'jpeg', quality=100)
     img_io.seek(0)
-
     img = base64.b64encode(img_io.getvalue())
-    
 
     return [res_str, img]
 
-##############
 
 # Web server
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
-# @app.route('/uploadfile', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             print('no file')
             return redirect(request.url)
+
         lang = str(request.form['lang'])
         file = request.files['file']
         
@@ -98,7 +94,6 @@ def upload_file():
         if file.filename == '':
             print('no filename')
             return redirect(request.url)
-        
 
         # stateless image
         if requests_queue.qsize() >= BATCH_SIZE:
@@ -113,7 +108,6 @@ def upload_file():
             time.sleep(CHECK_INTERVAL)
         [res, img] = req['output']
         return render_template('index.html', result=str(res), rawimg=img.decode('ascii'))
-            #return redirect(request.url)
     return render_template('index.html')
 
 
@@ -128,9 +122,6 @@ def word_extraction():
 
     try:
         PIL.Image.open(file).convert("RGB")
-        # base_image = Image.open(request.files['base_image'].stream)
-        # base_image.save("img.png")
-        # base_image = Image.open("img.png")
     except Exception:
         return {'error': 'please upload image file.'}, 400
 
@@ -147,14 +138,16 @@ def word_extraction():
     [res, img] = req['output']
     return str(res)
 
+
 @app.route('/healthz', methods=['GET'])
 def checkHealth():
 	return "Pong",200
 
+
 @app.errorhandler(413)
 def request_entity_too_large(error):
-    # return {'error': 'File Too Large'}, 413
     return render_template('index.html', result = 'The image size is too large'), 413
+
 
 if __name__ == '__main__':
     app.run(debug=False, port=8000, host='0.0.0.0', threaded=True)
