d05a08c70097a51c818b76b17ac6d355768405f8
Astro <astrocket.dev@gmail.com>
2022-02-21 01:10:49 +0900

    update

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def upload_file():
         temp['probability'] = section[2]
         result.append(temp)
 
-    return json.dumps(result, ensure_ascii=False), 200
+    return jsonify(result), 200
 
 @app.route('/health', methods=['GET'])
 def checkHealth():
